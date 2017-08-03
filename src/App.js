afc13a69173830daf5c49f1ef4a3f92e6e8ea067
Christine Gierer <beansprout@users.noreply.github.com>
2017-08-03 01:00:41 -0400

    imported BrowserRouter and wrapped App.js in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
-import { Home, About, Contact} from './components';
+import { Home, About, Contact } from './components';
 
-const App = () => (
-  <div>
-    <Navigation /> 
-  </div>
+const App = () => ( <
+    div >
+    <
+    Navigation / >
+    <
+    /div>
 )
 
 export default App;
